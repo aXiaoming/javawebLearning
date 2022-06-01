e4602f7c0f9b6bdd8f9c33c81abd30a884739899
xiaoming <a320976399@163.com>
2022-06-01 09:07:24 +0800

    B 使用switch-case语句对项目重构，合并多个servlet，对应pro13

--- pro13-fruit1.8-mvc/src/com/atguigu/fruit/servlets/FruitServlet.java
+++ pro13-fruit1.8-mvc/src/com/atguigu/fruit/servlets/FruitServlet.java
@@ -12,8 +12,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.List;
 
 @WebServlet("/fruit.do")
@@ -22,7 +20,31 @@ public class FruitServlet extends ViewBaseServlet {
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        //设置编码
+        request.setCharacterEncoding("UTF-8");
+        String operate = request.getParameter("operate");
+        if (StringUtil.isEmpty(operate)) {
+            operate = "index";
+        }
+        switch (operate) {
+            case "index":
+                index(request, response);
+                break;
+            case "add":
+                add(request, response);
+                break;
+            case "del":
+                del(request, response);
+                break;
+            case "edit":
+                edit(request, response);
+                break;
+            case "update":
+                update(request, response);
+                break;
+            default:
+                throw new RuntimeException("operate值非法!");
+        }
 
 
     }
@@ -42,7 +64,7 @@ public class FruitServlet extends ViewBaseServlet {
         String remark = request.getParameter("remark");
 
         //3.执行更新
-        fruitDAO.updateFruit(new Fruit(fid,fname, price ,fcount ,remark ));
+        fruitDAO.updateFruit(new Fruit(fid, fname, price, fcount, remark));
 
         //4.资源跳转
         //super.processTemplate("index",request,response);
@@ -51,19 +73,19 @@ public class FruitServlet extends ViewBaseServlet {
         response.sendRedirect("fruit.do");
     }
 
-    private void edit(HttpServletRequest request , HttpServletResponse response)throws IOException, ServletException {
+    private void edit(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         String fidStr = request.getParameter("fid");
-        if(StringUtil.isNotEmpty(fidStr)){
+        if (StringUtil.isNotEmpty(fidStr)) {
             int fid = Integer.parseInt(fidStr);
             Fruit fruit = fruitDAO.getFruitByFid(fid);
-            request.setAttribute("fruit",fruit);
-            super.processTemplate("edit",request,response);
+            request.setAttribute("fruit", fruit);
+            super.processTemplate("edit", request, response);
         }
     }
 
-    private void del(HttpServletRequest request , HttpServletResponse response)throws IOException, ServletException {
+    private void del(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         String fidStr = request.getParameter("fid");
-        if(StringUtil.isNotEmpty(fidStr)){
+        if (StringUtil.isNotEmpty(fidStr)) {
             int fid = Integer.parseInt(fidStr);
             fruitDAO.delFruit(fid);
 
@@ -76,11 +98,11 @@ public class FruitServlet extends ViewBaseServlet {
         request.setCharacterEncoding("UTF-8");
 
         String fname = request.getParameter("fname");
-        Integer price = Integer.parseInt(request.getParameter("price")) ;
+        Integer price = Integer.parseInt(request.getParameter("price"));
         Integer fcount = Integer.parseInt(request.getParameter("fcount"));
         String remark = request.getParameter("remark");
 
-        Fruit fruit = new Fruit(0,fname , price , fcount , remark ) ;
+        Fruit fruit = new Fruit(0, fname, price, fcount, remark);
 
         fruitDAO.addFruit(fruit);
 
@@ -88,55 +110,55 @@ public class FruitServlet extends ViewBaseServlet {
 
     }
 
-    private void index(HttpServletRequest request , HttpServletResponse response)throws IOException, ServletException {
-        HttpSession session = request.getSession() ;
+    private void index(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        HttpSession session = request.getSession();
 
         // 设置当前页，默认值1
-        Integer pageNo = 1 ;
+        Integer pageNo = 1;
 
         String oper = request.getParameter("oper");
 
         //如果oper!=null 说明 通过表单的查询按钮点击过来的
         //如果oper是空的，说明 不是通过表单的查询按钮点击过来的
-        String keyword = null ;
-        if(StringUtil.isNotEmpty(oper) && "search".equals(oper)){
+        String keyword;
+        if (StringUtil.isNotEmpty(oper) && "search".equals(oper)) {
             //说明是点击表单查询发送过来的请求
             //此时，pageNo应该还原为1 ， keyword应该从请求参数中获取
-            pageNo = 1 ;
+            pageNo = 1;
             keyword = request.getParameter("keyword");
             //如果keyword为null，需要设置为空字符串""，否则查询时会拼接成 %null% , 我们期望的是 %%
-            if(StringUtil.isEmpty(keyword)){
-                keyword = "" ;
+            if (StringUtil.isEmpty(keyword)) {
+                keyword = "";
             }
             //将keyword保存（覆盖）到session中
-            session.setAttribute("keyword",keyword);
-        }else{
+            session.setAttribute("keyword", keyword);
+        } else {
             //说明此处不是点击表单查询发送过来的请求（比如点击下面的上一页下一页或者直接在地址栏输入网址）
             //此时keyword应该从session作用域获取
             String pageNoStr = request.getParameter("pageNo");
-            if(StringUtil.isNotEmpty(pageNoStr)){
+            if (StringUtil.isNotEmpty(pageNoStr)) {
                 pageNo = Integer.parseInt(pageNoStr);   //如果从请求中读取到pageNo，则类型转换。否则，pageNo默认就是1
             }
             //如果不是点击的查询按钮，那么查询是基于session中保存的现有keyword进行查询
             Object keywordObj = session.getAttribute("keyword");
-            if(keywordObj!=null){
-                keyword = (String)keywordObj ;
-            }else{
-                keyword = "" ;
+            if (keywordObj != null) {
+                keyword = (String) keywordObj;
+            } else {
+                keyword = "";
             }
         }
 
         // 重新更新当前页的值
-        session.setAttribute("pageNo",pageNo);
+        session.setAttribute("pageNo", pageNo);
 
         FruitDAO fruitDAO = new FruitDAOImpl();
-        List<Fruit> fruitList = fruitDAO.getFruitList(keyword , pageNo);
-        session.setAttribute("fruitList",fruitList);
+        List<Fruit> fruitList = fruitDAO.getFruitList(keyword, pageNo);
+        session.setAttribute("fruitList", fruitList);
 
         //总记录条数
         int fruitCount = fruitDAO.getFruitCount(keyword);
         //总页数
-        int pageCount = (fruitCount+5-1)/5 ;
+        int pageCount = (fruitCount + 5 - 1) / 5;
         /*
         总记录条数       总页数
         1               1
@@ -146,13 +168,13 @@ public class FruitServlet extends ViewBaseServlet {
         11              3
         fruitCount      (fruitCount+5-1)/5
          */
-        session.setAttribute("pageCount",pageCount);
+        session.setAttribute("pageCount", pageCount);
 
         //此处的视图名称是 index
         //那么thymeleaf会将这个 逻辑视图名称 对应到 物理视图 名称上去
         //逻辑视图名称 ：   index
         //物理视图名称 ：   view-prefix + 逻辑视图名称 + view-suffix
         //所以真实的视图名称是：      /       index       .html
-        super.processTemplate("index",request,response);
+        super.processTemplate("index", request, response);
     }
 }
