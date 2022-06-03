36e1c3cd99df61da2aebc3e7fb4475df2bb9e7a8
xiaoming <a320976399@163.com>
2022-06-03 18:13:52 +0800

    B 在dispatcherservlet中添加统一获取参数、 视图跳转，对应pro16

--- pro16-fruit2.1-mvc-controller/src/com/atguigu/fruit/controllers/FruitController.java
+++ pro16-fruit2.1-mvc-controller/src/com/atguigu/fruit/controllers/FruitController.java
@@ -14,71 +14,71 @@ import java.util.List;
 public class FruitController {
     private FruitDAO fruitDAO = new FruitDAOImpl();
 
-    private String update(Integer fid , String fname , Integer price , Integer fcount , String remark ){
+    private String update(Integer fid, String fname, Integer price, Integer fcount, String remark) {
         //3.执行更新
-        fruitDAO.updateFruit(new Fruit(fid,fname, price ,fcount ,remark ));
+        fruitDAO.updateFruit(new Fruit(fid, fname, price, fcount, remark));
         //4.资源跳转
         return "redirect:fruit.do";
     }
 
-    private String edit(Integer fid , HttpServletRequest request){
-        if(fid!=null){
+    private String edit(Integer fid, HttpServletRequest request) {
+        if (fid != null) {
             Fruit fruit = fruitDAO.getFruitByFid(fid);
-            request.setAttribute("fruit",fruit);
+            request.setAttribute("fruit", fruit);
             //super.processTemplate("edit",request,response);
             return "edit";
         }
-        return "error" ;
+        return "error";
     }
 
-    private String del(Integer fid  ){
-        if(fid!=null){
+    private String del(Integer fid) {
+        if (fid != null) {
             fruitDAO.delFruit(fid);
             return "redirect:fruit.do";
         }
         return "error";
     }
 
-    private String add(String fname , Integer price , Integer fcount , String remark ) {
-        Fruit fruit = new Fruit(0,fname , price , fcount , remark ) ;
+    private String add(String fname, Integer price, Integer fcount, String remark) {
+        Fruit fruit = new Fruit(0, fname, price, fcount, remark);
         fruitDAO.addFruit(fruit);
         return "redirect:fruit.do";
     }
 
-    private String index(String oper , String keyword , Integer pageNo , HttpServletRequest request ) {
-        HttpSession session = request.getSession() ;
+    private String index(String oper, String keyword, Integer pageNo, HttpServletRequest request) {
+        HttpSession session = request.getSession();
 
-        if(pageNo==null){
+        if (pageNo == null) {
             pageNo = 1;
         }
-        if(StringUtil.isNotEmpty(oper) && "search".equals(oper)){
-            pageNo = 1 ;
-            if(StringUtil.isEmpty(keyword)){
-                keyword = "" ;
+        if (StringUtil.isNotEmpty(oper) && "search".equals(oper)) {
+            pageNo = 1;
+            if (StringUtil.isEmpty(keyword)) {
+                keyword = "";
             }
-            session.setAttribute("keyword",keyword);
-        }else{
+            session.setAttribute("keyword", keyword);
+        } else {
             Object keywordObj = session.getAttribute("keyword");
-            if(keywordObj!=null){
-                keyword = (String)keywordObj ;
-            }else{
-                keyword = "" ;
+            if (keywordObj != null) {
+                keyword = (String) keywordObj;
+            } else {
+                keyword = "";
             }
         }
 
         // 重新更新当前页的值
-        session.setAttribute("pageNo",pageNo);
+        session.setAttribute("pageNo", pageNo);
 
         FruitDAO fruitDAO = new FruitDAOImpl();
-        List<Fruit> fruitList = fruitDAO.getFruitList(keyword , pageNo);
-        session.setAttribute("fruitList",fruitList);
+        List<Fruit> fruitList = fruitDAO.getFruitList(keyword, pageNo);
+        session.setAttribute("fruitList", fruitList);
 
         //总记录条数
         int fruitCount = fruitDAO.getFruitCount(keyword);
         //总页数
-        int pageCount = (fruitCount+5-1)/5 ;
-        session.setAttribute("pageCount",pageCount);
+        int pageCount = (fruitCount + 5 - 1) / 5;
+        session.setAttribute("pageCount", pageCount);
 
-        return "index" ;
+        return "index";
     }
 }
--- pro16-fruit2.1-mvc-controller/src/com/atguigu/myssm/myspringmvc/DispatcherServlet.java
+++ pro16-fruit2.1-mvc-controller/src/com/atguigu/myssm/myspringmvc/DispatcherServlet.java
@@ -24,11 +24,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 @WebServlet("*.do")
-public class DispatcherServlet extends ViewBaseServlet{
+public class DispatcherServlet extends ViewBaseServlet {
 
-    private Map<String,Object> beanMap = new HashMap<>();
+    private Map<String, Object> beanMap = new HashMap<>();
 
-    public DispatcherServlet(){
+    public DispatcherServlet() {
     }
 
     public void init() throws ServletException {
@@ -38,21 +38,21 @@ public class DispatcherServlet extends ViewBaseServlet{
             //1.创建DocumentBuilderFactory
             DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
             //2.创建DocumentBuilder对象
-            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder() ;
+            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
             //3.创建Document对象
             Document document = documentBuilder.parse(inputStream);
 
             //4.获取所有的bean节点
             NodeList beanNodeList = document.getElementsByTagName("bean");
-            for(int i = 0 ; i<beanNodeList.getLength() ; i++){
+            for (int i = 0; i < beanNodeList.getLength(); i++) {
                 Node beanNode = beanNodeList.item(i);
-                if(beanNode.getNodeType() == Node.ELEMENT_NODE){
-                    Element beanElement = (Element)beanNode ;
-                    String beanId =  beanElement.getAttribute("id");
+                if (beanNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element beanElement = (Element) beanNode;
+                    String beanId = beanElement.getAttribute("id");
                     String className = beanElement.getAttribute("class");
                     Class controllerBeanClass = Class.forName(className);
-                    Object beanObj = controllerBeanClass.newInstance() ;
-                    beanMap.put(beanId , beanObj) ;
+                    Object beanObj = controllerBeanClass.newInstance();
+                    beanMap.put(beanId, beanObj);
                 }
             }
         } catch (ParserConfigurationException e) {
@@ -81,20 +81,20 @@ public class DispatcherServlet extends ViewBaseServlet{
         // 第2步： hello -> HelloController 或者 fruit -> FruitController
         String servletPath = request.getServletPath();
         servletPath = servletPath.substring(1);
-        int lastDotIndex = servletPath.lastIndexOf(".do") ;
-        servletPath = servletPath.substring(0,lastDotIndex);
+        int lastDotIndex = servletPath.lastIndexOf(".do");
+        servletPath = servletPath.substring(0, lastDotIndex);
 
         Object controllerBeanObj = beanMap.get(servletPath);
 
         String operate = request.getParameter("operate");
-        if(StringUtil.isEmpty(operate)){
-            operate = "index" ;
+        if (StringUtil.isEmpty(operate)) {
+            operate = "index";
         }
 
         try {
             Method[] methods = controllerBeanObj.getClass().getDeclaredMethods();
-            for(Method method : methods){
-                if(operate.equals(method.getName())){
+            for (Method method : methods) {
+                if (operate.equals(method.getName())) {
                     //1.统一获取请求参数
 
                     //1-1.获取当前方法的参数，返回参数数组
@@ -103,41 +103,42 @@ public class DispatcherServlet extends ViewBaseServlet{
                     Object[] parameterValues = new Object[parameters.length];
                     for (int i = 0; i < parameters.length; i++) {
                         Parameter parameter = parameters[i];
-                        String parameterName = parameter.getName() ;
+                        String parameterName = parameter.getName();
                         //如果参数名是request,response,session 那么就不是通过请求中获取参数的方式了
-                        if("request".equals(parameterName)){
-                            parameterValues[i] = request ;
-                        }else if("response".equals(parameterName)){
-                            parameterValues[i] = response ;
-                        }else if("session".equals(parameterName)){
-                            parameterValues[i] = request.getSession() ;
-                        }else{
+                        if ("request".equals(parameterName)) {
+                            parameterValues[i] = request;
+                        } else if ("response".equals(parameterName)) {
+                            parameterValues[i] = response;
+                        } else if ("session".equals(parameterName)) {
+                            parameterValues[i] = request.getSession();
+                        } else {
                             //从请求中获取参数值
                             String parameterValue = request.getParameter(parameterName);
                             String typeName = parameter.getType().getName();
 
-                            Object parameterObj = parameterValue ;
+                            Object parameterObj = parameterValue;
 
-                            if(parameterObj!=null) {
+                            if (parameterObj != null) {
                                 if ("java.lang.Integer".equals(typeName)) {
                                     parameterObj = Integer.parseInt(parameterValue);
                                 }
                             }
 
-                            parameterValues[i] = parameterObj ;
+                            parameterValues[i] = parameterObj;
                         }
                     }
+
                     //2.controller组件中的方法调用
                     method.setAccessible(true);
-                    Object returnObj = method.invoke(controllerBeanObj,parameterValues);
+                    Object returnObj = method.invoke(controllerBeanObj, parameterValues);
 
                     //3.视图处理
-                    String methodReturnStr = (String)returnObj ;
-                    if(methodReturnStr.startsWith("redirect:")){        //比如：  redirect:fruit.do
+                    String methodReturnStr = (String) returnObj;
+                    if (methodReturnStr.startsWith("redirect:")) {        //比如：  redirect:fruit.do
                         String redirectStr = methodReturnStr.substring("redirect:".length());
                         response.sendRedirect(redirectStr);
-                    }else{
-                        super.processTemplate(methodReturnStr,request,response);    // 比如：  "edit"
+                    } else {
+                        super.processTemplate(methodReturnStr, request, response);    // 比如：  "edit"
                     }
                 }
             }
