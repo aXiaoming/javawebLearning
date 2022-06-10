1a4e88654f3e2f536c84cadc11960e5dab2b7fc5
xiaoming <a320976399@163.com>
2022-06-10 09:17:06 +0800

    L 1.进入好友空间、返回自己空间的实现，对应pro22

--- pro21-qqzone1.0/src/com/atguigu/myssm/myspringmvc/PageController.java
+++ pro21-qqzone1.0/src/com/atguigu/myssm/myspringmvc/PageController.java
@@ -1,6 +1,8 @@
 package com.atguigu.myssm.myspringmvc;
 
 public class PageController {
+
+    // http://localhost:8080/pro21/page.do?page=index
     public String page(String page){
         return page ;       // frames/left
     }
